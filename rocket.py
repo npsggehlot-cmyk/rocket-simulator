7a3709e0047412a0ca12e063f5b791649f189981
Naivedhya Gehlot <npsggehlot@gmail.com>
2026-05-25 00:14:52 -0400

    added aerodynamic drag to both burn and coast phases

--- rocket.py
+++ rocket.py
@@ -1,3 +1,5 @@
+import math
+
 name = "Falcon"
 mass = 0.5
 diameter = 0.05
@@ -11,6 +13,10 @@ velocity = 0
 altitude = 0
 time = 0
 peak_altitude = 0
+cd = 0.5
+air_density = 1.225
+area = math.pi * (diameter/2) ** 2
+
 
 print("Rocket:", name)
 print("Mass:", mass, "kg")
@@ -19,6 +25,8 @@ print("Net Force:", round(net_force, 1), "N")
 print("Acceleration:", round(acceleration, 1), "m/s²")
 
 while time < burn_time:
+    drag = 0.5 * cd * air_density * area * velocity ** 2
+    acceleration = (thrust - drag - weight) / mass
     velocity = velocity + acceleration * dt
     altitude = altitude + velocity * dt
     time = time + dt
@@ -29,13 +37,15 @@ print("Velocity at burnout:", round(velocity, 1), "m/s")
 while altitude > 0:
     if altitude > peak_altitude:
         peak_altitude = altitude
-    acceleration = -weight / mass
+    drag = 0.5 * cd * air_density * area * velocity ** 2
+    acceleration = (-weight - drag * math.copysign(1, velocity)) / mass
     velocity = velocity + acceleration * dt
-    altitude = altitude + velocity * dt
+    altitude = altitude + velocity * dt 
     time = time + dt
 
 print("Altitude at landing:", round(altitude, 1), "m")
 print("Velocity at landing:", round(velocity, 1), "m/s")
 print("Peak altitude:", round(peak_altitude, 1), "m")
+print("Area:", round(area, 3), "m²")
 
 
